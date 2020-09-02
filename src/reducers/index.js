ae36a08c8fa86daa87f07c549c2be9a4d5e75bdd
brunoPedrosa <brunofellipep@gmail.com>
2020-09-02 00:15:53 -0300

    fetchusers done and connected with userheader component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from "redux";
 
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
+
 export default combineReducers({
-    replaceMe: () => 'to be replaced'
+  posts: postsReducer,
+  users: usersReducer
 });
